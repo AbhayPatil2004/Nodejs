95a5f741556bcf5ee5fbe4ac793e7ad42bbb5979
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-11-25 15:56:27 +0530

    import.meta

--- ES6/app.js
+++ ES6/app.js
@@ -1,3 +1,17 @@
 import  num , {a } from './math.js'
 
 console.log(num , a)
+
+console.log( import.meta)
+console.log( import.meta.url)
+ 
+const { filename , dirname } = import.meta
+
+console.log( filename)
+console.log( dirname)
+
+import.meta.a = 'Abhay'
+const name = import.meta.a 
+console.log(name)
+
+console.log( process.cwd())
